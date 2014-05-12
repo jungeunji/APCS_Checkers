fd2f80b441726e4f89803b82ec656727ae722974
jungeunji <garussss@gmail.com>
2014-05-12 09:09:52 -0700

    Added gridworld.jar

--- Checkers/CheckerPlayer.java
+++ Checkers/CheckerPlayer.java
@@ -1,3 +1,8 @@
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+import java.util.ArrayList;
+import java.awt.Color;
+
 /**
  * A CheckerPlayer class that contains the basic functionalities of
  * a player in the Checkers game.
@@ -5,6 +10,11 @@
  * @author Darren Yang
  * @version May 12, 2014
  */
-public abstract class CheckerPlayer {
-
+public abstract class CheckerPlayer 
+{
+	//The world
+	private CheckerWorld world;
+	
+	//The grid
+	private 
 }
