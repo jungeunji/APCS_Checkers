6a2d53b22afaf8e41fb6a60fecfd411514c474b6
jungeunji <garussss@gmail.com>
2014-05-14 09:50:38 -0700

    Finished Piece

--- Checkers/Piece.java
+++ Checkers/Piece.java
@@ -1,10 +1,33 @@
 import java.awt.Color;
 
-
 public class Piece 
 {
+	private Color color;
+	private boolean isKing;
+	
 	public Piece(Color color)
 	{
-		
+		this.color = color;
+		isKing = false;
+	}
+	
+	public Color getColor()
+	{
+		return color;
+	}
+	
+	public void setColor( Color newColor )
+	{
+		color = newColor;
+	}
+	
+	public void setKing()
+	{
+		isKing = true;
+	}
+	
+	public boolean king()
+	{
+		return isKing;
 	}
 }
